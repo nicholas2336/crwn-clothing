458360a7d3da1eb34875394beefafde756c301a9
Nicholas A <bobby.nicholas23@gmail.com>
2022-02-15 13:25:22 +0900

    updating stripe integration and updating our fasvicon and title

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -5,6 +5,8 @@ import CheckoutItem from '../../components/checkout-item/checkout-item.component
 
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selector';
 
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
+
 import './checkout.styles.scss';
 
 const CheckoutPage = ({ cartItems, total }) => (
@@ -30,6 +32,12 @@ const CheckoutPage = ({ cartItems, total }) => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
     <div className='total'>TOTAL: ${total}</div>
+    <div className='test-warning'>
+      *Please use the following test credit card for Payment*
+      <br/>
+      4242 4242 4242 4242 - Exp: 12/25 - CVV: 123
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
